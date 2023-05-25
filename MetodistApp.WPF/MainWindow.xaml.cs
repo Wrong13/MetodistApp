4f9e01b7c95124ddcd4dd9bd4829b131d91f9620
Wrong13 <ilak71363@gmail.com>
2023-05-25 17:13:25 +0300

    Переделал под MVVM Смог сделать ассинхронные команды (https://learn.microsoft.com/ru-ru/archive/msdn-magazine/2014/april/async-programming-patterns-for-asynchronous-mvvm-applications-commands)

--- MetodistApp.WPF/MainWindow.xaml.cs
+++ MetodistApp.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Models;
+using MetodistApp.WPF.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,36 +25,10 @@ namespace MetodistApp.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        public IEnumerable<Tutor> Tutors;
-        MyApiClient apiClient;
         public MainWindow()
         {
             InitializeComponent();
-            apiClient = new MyApiClient();
-            LoadTut();
-        }
-        private async void LoadTut()
-        {
-            Tutors = await apiClient.GetTutors();
-            MainView.ItemsSource = Tutors;
-        }
-
-        private async void DelBtn_Click(object sender, RoutedEventArgs e)
-        {
-            var DelTutor = MainView.SelectedItem as Tutor;
-            if (DelTutor == null)
-                return;
-            await apiClient.DeleteTutor(DelTutor.Id);
-            LoadTut();
-        }
-
-        private void EditBtn_Click(object sender, RoutedEventArgs e)
-        {
-            var DelTutor = MainView.SelectedItem as Tutor;
-            if (DelTutor == null)
-                return;
-            EditTutorWindow editTutorWindow = new EditTutorWindow(DelTutor);
-            editTutorWindow.Show();
+            DataContext = new MainVm();
         }
     }
 }
---
+++ MetodistApp.WPF/ViewModel/MainVm.cs
@@ -0,0 +1,102 @@
+﻿using Domain.Models;
+using MetodistApp.WPF.ViewModel.AsyncCmd;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodistApp.WPF.ViewModel
+{
+    class MainVm : INotifyPropertyChanged
+    {
+        Tutor selectedTutor;
+        public Tutor SelectedTutor
+        {
+            get { return selectedTutor; }
+            set { selectedTutor = value;}
+        }
+        IEnumerable<Tutor> tutors;
+        public IEnumerable<Tutor> Tutors
+        {
+            get { return tutors; }
+            set { tutors = value; OnPropertyChanged(nameof(Tutors)); }
+        }
+        
+
+
+        AsyncCommand editTutor;
+        AsyncCommand deleteTuror;
+        AsyncCommand addTutor;
+
+        MyApiClient apiClient;
+        public MainVm()
+        {
+            apiClient = new MyApiClient();
+            LoadTut();
+        }
+
+        public AsyncCommand EditTutor
+        {
+            get
+            {
+                return editTutor ?? (editTutor = new AsyncCommand(async () =>
+                {
+                    
+                    if (SelectedTutor == null)
+                    {
+                        LoadTut();
+                        return;
+                    }
+                    EditTutorWindow editTutorWindow = new EditTutorWindow(SelectedTutor);
+                    editTutorWindow.ShowDialog();
+                    LoadTut();
+                }));
+            }
+        }
+
+        public AsyncCommand AddTutor
+        {
+            get
+            {
+                return addTutor ?? (addTutor = new AsyncCommand(async () =>
+                {
+                    EditTutorWindow editTutorWindow = new EditTutorWindow(new Tutor(),false);
+                    editTutorWindow.ShowDialog();
+                    LoadTut();
+                }));
+            }
+        }
+
+        public AsyncCommand DeleteTutor
+        {
+            get
+            {
+                return deleteTuror ?? (deleteTuror = new AsyncCommand(async () =>
+                {
+                    if (SelectedTutor == null)
+                        return;
+                    await apiClient.DeleteTutor(SelectedTutor.Id);
+                    LoadTut();
+                }));
+            }
+        }
+
+        private async void LoadTut()
+        {
+            Tutors = await apiClient.GetTutors();
+            OnPropertyChanged(nameof(Tutors));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = " ")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
